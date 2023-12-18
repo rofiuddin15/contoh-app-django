d0ac41838a9344df11b6d985255add452b18d086
rofiareiv <kambing.rofi@gmail.com>
2023-12-18 10:27:12 +0700

    ada perubahan pada forms.py

--- berita/forms.py
+++ berita/forms.py
@@ -1,13 +1,5 @@
 from django import forms
 
-CHOICES =( 
-    ("1", "One"), 
-    ("2", "Two"), 
-    ("3", "Three"), 
-    ("4", "Four"), 
-    ("5", "Five"), 
-)
-
 class BeritaForm(forms.Form):
     judul = forms.CharField(help_text='tulis judul disini', max_length=100)
     konten = forms.CharField(max_length=100)
